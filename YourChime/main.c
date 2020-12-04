07c3abfaf6208a1094a4cfe26f35aafec396f305
Michael G. Hale <michaelghaleccc@gmail.com>
2020-12-04 19:55:19 +0000

    inital commit

---
+++ YourChime/main.c
@@ -0,0 +1,18 @@
+/*
+ * YourChime.c
+ *
+ * Created: 04/12/2020 19:49:31
+ * Author : mghgp
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
