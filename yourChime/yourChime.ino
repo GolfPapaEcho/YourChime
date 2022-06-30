96e41f649a8df85464f8964ada64f7941e9bb77e
michaelghale <michaelghaleccc@gmail.com>
2022-06-30 19:51:30 +0100

    added third function

--- yourChime/yourChime.ino
+++ yourChime/yourChime.ino
@@ -1,29 +1,33 @@
 //yourChime Arduino Sketch by Michael G. Hale 18.05.22 
 // constants won't change. They're used here to set pin numbers:
-const int etPin = 2;        // the number of the egg/tea pin
-const int medPin = 4;// the number of the meditation pin
-const int solenoidPin = 13; // the number of the solenoid pin
+const byte etPin = 2;        // the number of the egg/tea pin
+const byte medPin = 4;// the number of the meditation pin
+const byte solenoidPin = 13; // the number of the solenoid pin
 
 // Variables will change:
 //int solenoidState = LOW;         // the current state of the output pin
-int etButtonState;                // the current etReading from the input pin
-int medButtonState;               // current medReading 
-int lastEtButtonState = LOW;   // the previous etReading from the input pin
-int lastMedButtonState = LOW;  //previous medRading
+byte etButtonState;                // the current etReading from the input pin
+byte medButtonState;               // current medReading 
+byte lastEtButtonState = 0;   // the previous etReading from the input pin
+byte lastMedButtonState = 1;  //previous medRading
  
 // the following variables are unsigned longs because the time, measured in
 // milliseconds, will quickly become a bigger number than can be stored in an int.
 unsigned long lastEtDebounceTime = 0;
 unsigned long lastMedDebounceTime = 0; // the last time the output pin was toggsolenoid
 unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
-
+unsigned long interButtonDelay = 2000;
+bool etBool;
+bool medBool;  
+  
   // trigger solenoid:
   int fireSolenoid(){
     digitalWrite(solenoidPin, HIGH);
     unsigned long timer1 = millis();
-    while (millis() - timer1 < 200){}
+    while (millis() - timer1 < 100){}
     digitalWrite(solenoidPin, LOW);
   }
+
 
 void setup() {
   pinMode(etPin, INPUT);
@@ -31,7 +35,7 @@ void setup() {
   pinMode(solenoidPin, OUTPUT);
 
   // set initial solenoid state
-  digitalWrite(solenoidPin, LOW);
+  digitalWrite(solenoidPin, 0);
 
 
   
@@ -39,8 +43,8 @@ void setup() {
 
 void loop() {
   // read the state of the switches into a local variables:
-  int etReading = digitalRead(etPin);
-  int medReading = digitalRead(medPin);
+  byte etReading = digitalRead(etPin);
+  byte medReading = digitalRead(medPin);
   // check to see if you just pressed a button
   // (i.e. the input went from LOW to HIGH), and you've waited long enough
   // since the last press to ignore any noise:
@@ -63,8 +67,9 @@ void loop() {
       etButtonState = etReading;
 
       // only toggle the solenoid if the new button state is HIGH
-      if (etButtonState == HIGH) {
+      if (etButtonState == 1) {
          fireSolenoid();
+         etBool = true;
       }
     }
   }
@@ -78,14 +83,26 @@ void loop() {
       medButtonState = medReading;
 
       // only toggle the solenoid if the new button state is HIGH
-      if (medButtonState == HIGH) {
+      if (medButtonState == 1) {
         fireSolenoid();
+        medBool = true;
       }
     }
   }
 
-  
-  
+  if ((millis() - lastEtDebounceTime > interButtonDelay) || (millis() - lastMedDebounceTime > interButtonDelay)){
+    etBool = false;
+    medBool = false;
+  }
+
+  if (etBool && medBool){
+    unsigned long timer2 = millis();
+    fireSolenoid();
+    while (millis() - timer2 < 255){}
+    fireSolenoid();
+    while (millis() - timer2 < 355){}
+    fireSolenoid();
+    }
   // save the Reading. Next time through the loop, it'll be the lastEt/MedButtonState:
   lastEtButtonState = etReading;
   lastMedButtonState = medReading;
